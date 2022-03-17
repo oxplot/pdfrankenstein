6d6a4529fe27fca8d8ae5c72282c164769092eb1
Mansour Behabadi <mansour@oxplot.com>
2022-03-18 00:15:01 +1100

    Split annotation machinery into its own package

--- main.go
+++ main.go
@@ -1,349 +1,26 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/json"
-	"encoding/xml"
-	"errors"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"regexp"
 	"strconv"
 	"strings"
-	"text/template"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
+
+	"github.com/oxplot/pdfrankestein/session"
 )
 
 const (
 	progName = "PDFrankestein"
 )
 
-var (
-	srcBGPat = regexp.MustCompile(`<image[^>]*id="src-bg"[^>]*>`)
-	annotTpl = template.Must(template.New("").Funcs(map[string]any{
-		"stripunit": func(v string) string {
-			return strings.TrimRight(v, "x%npiemtc")
-		},
-	}).Parse(`<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg
-   width="{{.Width}}"
-   height="{{.Height}}"
-   viewBox="{{.ViewBox}}"
-   version="1.1"
-   xmlns:xlink="http://www.w3.org/1999/xlink"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   xmlns:svg="http://www.w3.org/2000/svg">
-  <g
-     inkscape:label="Layer 1"
-     inkscape:groupmode="layer"
-     id="layer1">
-    <image
-       id="src-bg"
-       preserveAspectRatio="none"
-       width="{{.Width | stripunit}}"
-       height="{{.Height | stripunit}}"
-       style="image-rendering:optimizeQuality"
-       xlink:href="{{.Href}}"
-       sodipodi:insensitive="true"
-       inkscape:svg-dpi="300"
-       x="0"
-       y="0" />
-  </g>
-</svg>
-`))
-
-	tmpRoot string
-)
-
-func cmdErr(err error) error {
-	var exitErr *exec.ExitError
-	if errors.As(err, &exitErr) {
-		return errors.New(string(exitErr.Stderr))
-	}
-	return err
-}
-
-func handleHome(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-}
-
-func getPageCount(path string) (int, error) {
-	out, err := exec.Command("qpdf", "--show-npages", path).Output()
-	if err != nil {
-		return 0, cmdErr(err)
-	}
-	p, err := strconv.Atoi(strings.TrimSpace(string(out)))
-	if err != nil {
-		return 0, fmt.Errorf("cannot convert page count: %s", err)
-	}
-	return p, nil
-}
-
-func calcPDFSignature(path string) (string, error) {
-	stat, err := os.Stat(path)
-	if err != nil {
-		return "", err
-	}
-	tail := make([]byte, 1024)
-	f, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-	s := stat.Size() - int64(len(tail))
-	if s < 0 {
-		s = 0
-	}
-	if _, err := f.Seek(s, 0); err != nil {
-		return "", err
-	}
-	if _, err := io.ReadFull(f, tail); err != nil {
-		return "", err
-	}
-	sign := sha256.Sum256([]byte(fmt.Sprintf("%s,%d,%d,%s", stat.Size(), stat.ModTime(), tail)))
-	return fmt.Sprintf("%x", sign), nil
-}
-
-func handleThumb(w http.ResponseWriter, r *http.Request) {
-
-	// Simple caching thumbnail generator:
-	// 1. Make a unique signature from the given PDF file.
-	// 2. Hash its last 1KB of data, its size and modified timestamp.
-	// 3. Check cache if the hash+page is already available.
-	// 3. If not, use inkscape to import the given PDF page and export it as a downsampled PNG.
-
-	w.Header().Add("Content-Type", "image/png")
-
-	path := r.URL.Query().Get("path")
-	page := r.URL.Query().Get("page")
-	hasBG := r.URL.Query().Get("bg") != ""
-	if _, err := strconv.Atoi(page); path == "" || err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	sign, err := calcPDFSignature(path)
-	if err != nil {
-		log.Printf("cannot calc PDF signature for %s: %s", path, err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	thumbPath := filepath.Join(tmpRoot, sign) + "-" + page
-	if hasBG {
-		thumbPath += "-bg"
-	}
-	thumbPath += ".png"
-
-	// Serve from cache if available
-
-	if b, err := os.ReadFile(thumbPath); err == nil {
-		w.Write(b)
-		return
-	}
-
-	// Run inkscape to generate image
-
-	exportOpacity := "0.0"
-	if hasBG {
-		exportOpacity = "1.0"
-	}
-
-	_ = os.MkdirAll(tmpRoot, 0750)
-	cmd := exec.Command("inkscape", "--pdf-page="+page, "--export-type=png",
-		"--export-area-page", "--export-dpi=20", "--pdf-poppler",
-		"--export-background=white", "--export-background-opacity="+exportOpacity,
-		"--export-filename="+thumbPath, path)
-	if _, err := cmd.Output(); err != nil {
-		log.Printf("failed to generate thumb for page %s of '%s' in '%s': %s", page, path, thumbPath, cmdErr(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Serve the image
-
-	b, err := os.ReadFile(thumbPath)
-	if err != nil {
-		log.Printf("failed to serve thumb '%s': %s", thumbPath, err)
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	w.Write(b)
-}
-
-// handleAnnotate will open Inkscape with the given page of the given PDF file
-// as a locked background over which the user can draw what they wish.
-// After saving and closing Inkscape, the HTTP request completes and the frontend
-// receives JSON {"annotated": bool, "path": string}. "annotated" is true if
-// the file was saved (based on its modified timestamp). "path" is a path to the
-// PDF of the annotated single page.
-func handleAnnotate(w http.ResponseWriter, r *http.Request) {
-
-	// 1. Inkscape export PDF page to SVG
-	// 2. Create a new SVG with (1) and instructions linked as background images
-	//    and locked.
-	// 3. Run Inkscape in GUI mode to edit (2).
-	// 4. Upon Inkscape exit, test if (2) was modified (based on filesystem timestamp).
-	// 5. If modified:
-	//    a. Update (2) removing the background images.
-	//    b. Export the (a) to a PDF in cache.
-	//    c. Return response to frontend.
-
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Header().Add("Content-Type", "application/json")
-
-	annotId := r.URL.Query().Get("id")
-	path := r.URL.Query().Get("path")
-	page := r.URL.Query().Get("page")
-	if _, err := strconv.Atoi(page); err != nil || path == "" || annotId == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	_ = os.MkdirAll(tmpRoot, 0750)
-
-	err := func() error {
-
-		// Export PDF page to SVG
-
-		srcPagePath := filepath.Join(tmpRoot, "annot-src-page-"+annotId+".svg")
-		cmd := exec.Command("inkscape", "--pdf-page="+page, "--export-type=svg",
-			"--pdf-poppler", "--export-filename="+srcPagePath, path)
-		if _, err := cmd.Output(); err != nil {
-			return fmt.Errorf("failed to convert page %s of '%s' to svg at '%s': %s", page, path, srcPagePath, err)
-		}
-
-		// Create a new SVG with above as background
-
-		svgPageSpecs := struct {
-			Width   string `xml:"width,attr"`
-			Height  string `xml:"height,attr"`
-			ViewBox string `xml:"viewBox,attr"`
-			Href    string `xml:"-"`
-		}{}
-		f, err := os.Open(srcPagePath)
-		if err != nil {
-			return fmt.Errorf("failed to open '%s': %s", srcPagePath, err)
-		}
-		if err := xml.NewDecoder(f).Decode(&svgPageSpecs); err != nil {
-			f.Close()
-			return fmt.Errorf("failed to convert page %s of '%s' to svg at '%s': %s", page, path, srcPagePath, err)
-		}
-		f.Close()
-
-		annotPath := filepath.Join(tmpRoot, "annot-"+annotId+".svg")
-		f, err = os.Create(annotPath)
-		if err != nil {
-			return fmt.Errorf("failed to create '%s': %s", annotPath, err)
-		}
-
-		svgPageSpecs.Href = srcPagePath
-		if err := annotTpl.Execute(f, svgPageSpecs); err != nil {
-			f.Close()
-			return fmt.Errorf("failed to write to '%s': %s", annotPath, err)
-		}
-		f.Close()
-
-		// Run Inkscape in GUI mode to edit the annotation file
-
-		beforeEditStat, err := os.Stat(annotPath)
-		if err != nil {
-			return fmt.Errorf("failed to stat '%s': %s", annotPath, err)
-		}
-
-		if _, err := exec.Command("inkscape", annotPath).Output(); err != nil {
-			return fmt.Errorf("inkscape exited with error while editing '%s': %s", annotPath, err)
-		}
-
-		afterEditStat, err := os.Stat(annotPath)
-		if err != nil {
-			return fmt.Errorf("failed to stat '%s': %s", annotPath, err)
-		}
-
-		// Unmodified annotation file results in immediate response back to client
-
-		if afterEditStat.ModTime() == beforeEditStat.ModTime() {
-			json.NewEncoder(w).Encode(struct {
-				Annotated bool `json:"annotated"`
-			}{})
-			return nil
-		}
-
-		// Otherwise, remove the background from the annotated file
-
-		b, err := ioutil.ReadFile(annotPath)
-		if err != nil {
-			return fmt.Errorf("failed to read back '%s': %s", annotPath, err)
-		}
-		b = srcBGPat.ReplaceAll(b, nil)
-		if err := ioutil.WriteFile(annotPath, b, 0644); err != nil {
-			return fmt.Errorf("failed to write back '%s': %s", annotPath, err)
-		}
-
-		// Convert the SVG annotation to PDF
-
-		annotPDFPath := filepath.Join(tmpRoot, "annot-"+annotId+".pdf")
-		cmd = exec.Command("inkscape", "--export-type=pdf", "--export-filename="+annotPDFPath, annotPath)
-		if _, err := cmd.Output(); err != nil {
-			return fmt.Errorf("failed to convert annotation SVG to PDF at '%s': %s", annotPDFPath, cmdErr(err))
-		}
-
-		json.NewEncoder(w).Encode(struct {
-			Annotated bool   `json:"annotated"`
-			Path      string `json:"path"`
-		}{true, annotPDFPath})
-
-		return nil
-	}()
-
-	if err != nil {
-		json.NewEncoder(w).Encode(struct {
-			Error string `json:"error"`
-		}{err.Error()})
-	}
-}
-
-func handleSave(w http.ResponseWriter, r *http.Request) {
-}
-
 func run() error {
-
-	// Init cache
-
-	tmpRoot, err := ioutil.TempDir("", "pdfrankestein-*")
-	if err != nil {
-		return err
-	}
-	defer func() {
-		// Cleanup temp
-		files, _ := ioutil.ReadDir(tmpRoot)
-		for _, f := range files {
-			_ = os.Remove(filepath.Join(tmpRoot, f.Name()))
-		}
-		_ = os.Remove(tmpRoot)
-	}()
-
-	// Init GUI
 
 	ap := app.New()
 	win := ap.NewWindow(progName)
@@ -355,6 +32,8 @@ func run() error {
 
 	var pages []*widget.Button
 	pageGrid := container.NewGridWrap(fyne.NewSize(100, 100))
+
+	var sess *session.Session
 
 	startContent := container.NewCenter(widget.NewButton("Open PDF File", func() {
 		dialog.ShowFileOpen(func(r fyne.URIReadCloser, err error) {
@@ -369,15 +48,16 @@ func run() error {
 				return
 			}
 			path = strings.TrimPrefix(path, "file://")
-			pageCount, err := getPageCount(path)
+
+			sess, err = session.New(path)
 			if err != nil {
 				dialog.ShowError(err, win)
 				return
 			}
-			pages = make([]*widget.Button, pageCount)
+			pages = make([]*widget.Button, sess.PageCount())
 			for i := range pages {
 				p := widget.NewButton("p."+strconv.Itoa(i+1), func() {
-					// TODO Annotate
+					sess.Annotate(i)
 				})
 				pages[i] = p
 				pageGrid.Add(p)
@@ -385,6 +65,7 @@ func run() error {
 			fileNameLabel.SetText(r.URI().Name())
 			filePathLabel.SetText(path)
 			win.SetContent(openedContent)
+
 		}, win)
 	}))
 
---
+++ session/session.go
@@ -0,0 +1,264 @@
+package session
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"text/template"
+)
+
+var (
+	srcBGPat = regexp.MustCompile(`<image[^>]*id="src-bg"[^>]*>`)
+	annotTpl = template.Must(template.New("").Funcs(map[string]any{
+		"stripunit": func(v string) string {
+			return strings.TrimRight(v, "x%npiemtc")
+		},
+	}).Parse(`<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   width="{{.Width}}"
+   height="{{.Height}}"
+   viewBox="{{.ViewBox}}"
+   version="1.1"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <image
+       id="src-bg"
+       preserveAspectRatio="none"
+       width="{{.Width | stripunit}}"
+       height="{{.Height | stripunit}}"
+       style="image-rendering:optimizeQuality"
+       xlink:href="{{.Href}}"
+       sodipodi:insensitive="true"
+       inkscape:svg-dpi="300"
+       x="0"
+       y="0" />
+  </g>
+</svg>
+`))
+)
+
+func cmdErr(err error) error {
+	var exitErr *exec.ExitError
+	if errors.As(err, &exitErr) {
+		return errors.New(string(exitErr.Stderr))
+	}
+	return err
+}
+
+type Session struct {
+	path      string
+	pageCount int
+	tmpDir    string
+}
+
+func New(path string) (*Session, error) {
+
+	// Get page count
+
+	out, err := exec.Command("qpdf", "--show-npages", path).Output()
+	if err != nil {
+		return nil, cmdErr(err)
+	}
+	p, err := strconv.Atoi(strings.TrimSpace(string(out)))
+	if err != nil {
+		return nil, fmt.Errorf("cannot convert page count: %s", err)
+	}
+
+	// Create temp dir
+
+	tmpDir, err := ioutil.TempDir("", "pdfrankestein-*")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp directory: %s", err)
+	}
+
+	return &Session{path: path, pageCount: p, tmpDir: tmpDir}, nil
+}
+
+func (s *Session) PageCount() int {
+	return s.pageCount
+}
+
+func (s *Session) Path() string {
+	return s.path
+}
+
+func (s *Session) Thumbnail(page int) (string, error) {
+
+	if page < 0 || page >= s.pageCount {
+		panic("invalid page number")
+	}
+
+	thumbPath := s.thumbPath(page)
+
+	// Serve from cache if available
+
+	if _, err := os.Stat(thumbPath); err == nil {
+		return thumbPath, nil
+	}
+
+	// Otherwise, run inkscape to generate image
+
+	cmd := exec.Command("inkscape", "--pdf-page="+strconv.Itoa(page), "--export-type=png",
+		"--export-area-page", "--export-dpi=20", "--pdf-poppler",
+		"--export-background=white", "--export-filename="+thumbPath+".tmp", s.path)
+	if _, err := cmd.Output(); err != nil {
+		return "", fmt.Errorf("failed to generate thumb for page %s of '%s': %s", page, s.path, cmdErr(err))
+	}
+	_ = os.Rename(thumbPath+".tmp", thumbPath)
+
+	return thumbPath, nil
+}
+
+func (s *Session) Annotate(page int) (bool, error) {
+
+	if page < 0 || page >= s.pageCount {
+		panic("invalid page number")
+	}
+
+	// Export PDF page to SVG (if needed)
+
+	srcPath := s.srcPath(page)
+	if _, err := os.Stat(srcPath); err != nil {
+		cmd := exec.Command("inkscape", "--pdf-page="+strconv.Itoa(page), "--export-type=svg",
+			"--pdf-poppler", "--export-filename="+srcPath+".tmp", s.path)
+		if _, err := cmd.Output(); err != nil {
+			return false, fmt.Errorf("failed to convert page %s of '%s' to svg: %s", page, s.path, cmdErr(err))
+		}
+		_ = os.Rename(srcPath+".tmp", srcPath)
+	}
+
+	// Create a new SVG with above as background (if needed)
+
+	annotPath := s.annotPath(page)
+	if _, err := os.Stat(annotPath); err != nil {
+		pageSpecs := struct {
+			Width   string `xml:"width,attr"`
+			Height  string `xml:"height,attr"`
+			ViewBox string `xml:"viewBox,attr"`
+			Href    string `xml:"-"`
+		}{}
+		f, err := os.Open(srcPath)
+		if err != nil {
+			return false, fmt.Errorf("failed to open '%s': %s", srcPath, err)
+		}
+		if err := xml.NewDecoder(f).Decode(&pageSpecs); err != nil {
+			f.Close()
+			return false, fmt.Errorf("failed to parse svg at '%s': %s", srcPath, err)
+		}
+		f.Close()
+
+		f, err = os.Create(annotPath + ".tmp")
+		if err != nil {
+			return false, fmt.Errorf("failed to create '%s': %s", annotPath, err)
+		}
+
+		pageSpecs.Href = srcPath
+		if err := annotTpl.Execute(f, pageSpecs); err != nil {
+			f.Close()
+			return false, fmt.Errorf("failed to write to '%s': %s", annotPath, err)
+		}
+		f.Close()
+		_ = os.Rename(annotPath+".tmp", annotPath)
+	}
+
+	// Run Inkscape in GUI mode to edit the annotation file
+
+	beforeEditStat, err := os.Stat(annotPath)
+	if err != nil {
+		return false, fmt.Errorf("failed to stat '%s': %s", annotPath, err)
+	}
+
+	if _, err := exec.Command("inkscape", annotPath).Output(); err != nil {
+		return false, fmt.Errorf("inkscape exited with error while editing '%s': %s", annotPath, err)
+	}
+
+	afterEditStat, err := os.Stat(annotPath)
+	if err != nil {
+		return false, fmt.Errorf("failed to stat '%s': %s", annotPath, err)
+	}
+
+	modified := afterEditStat.ModTime() != beforeEditStat.ModTime()
+	if modified {
+		_ = os.Remove(s.thumbPath(page))
+	}
+	return modified, nil
+}
+
+func (s *Session) annotPath(page int) string {
+	return filepath.Join(s.tmpDir, fmt.Sprintf("annot-%d.svg", page))
+}
+
+func (s *Session) srcPath(page int) string {
+	return filepath.Join(s.tmpDir, fmt.Sprintf("src-%d.svg", page))
+}
+
+func (s *Session) thumbPath(page int) string {
+	return filepath.Join(s.tmpDir, fmt.Sprintf("thumb-%d.png", page))
+}
+
+func (s *Session) IsAnnotated(page int) bool {
+	if page < 0 || page >= s.pageCount {
+		panic("invalid page number")
+	}
+	_, err := os.Stat(s.annotPath(page))
+	return err == nil
+}
+
+func (s *Session) Clear(page int) {
+	if page < 0 || page >= s.pageCount {
+		panic("invalid page number")
+	}
+	_ = os.Remove(s.annotPath(page))
+	_ = os.Remove(s.thumbPath(page))
+}
+
+func (s *Session) Save(path string) error {
+	/*
+		// Otherwise, remove the background from the annotated file
+
+		b, err := ioutil.ReadFile(annotPath)
+		if err != nil {
+			return fmt.Errorf("failed to read back '%s': %s", annotPath, err)
+		}
+		b = srcBGPat.ReplaceAll(b, nil)
+		if err := ioutil.WriteFile(annotPath, b, 0644); err != nil {
+			return fmt.Errorf("failed to write back '%s': %s", annotPath, err)
+		}
+
+		// Convert the SVG annotation to PDF
+
+		annotPDFPath := filepath.Join(tmpRoot, "annot-"+annotId+".pdf")
+		cmd = exec.Command("inkscape", "--export-type=pdf", "--export-filename="+annotPDFPath, annotPath)
+		if _, err := cmd.Output(); err != nil {
+			return fmt.Errorf("failed to convert annotation SVG to PDF at '%s': %s", annotPDFPath, cmdErr(err))
+		}
+
+		json.NewEncoder(w).Encode(struct {
+			Annotated bool   `json:"annotated"`
+			Path      string `json:"path"`
+		}{true, annotPDFPath})
+	*/
+	return nil
+}
+
+func (s *Session) Close() {
+	files, _ := ioutil.ReadDir(s.tmpDir)
+	for _, f := range files {
+		_ = os.Remove(filepath.Join(s.tmpDir, f.Name()))
+	}
+	_ = os.Remove(s.tmpDir)
+}
